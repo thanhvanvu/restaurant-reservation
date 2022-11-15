0556c4574c36b0a187fc96b9ed00fd379e14174a
SashTAl8 <93013511+Rubydang2292@users.noreply.github.com>
2022-11-15 00:48:06 -0600

    Ruby commit
    create an env file, create MongoDB online and connect server with MongoDB

---
+++ back-end/config/db.js
@@ -0,0 +1,18 @@
+// npm i mongoose
+
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    const uri = process.env.DB_URI
+    const connect = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+module.exports = { connectDB }
--- back-end/server.js
+++ back-end/server.js
@@ -1,18 +1,42 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
 
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      reservations: [],
-    },
+//config dotenv
+require('dotenv').config()
+
+// function to boot server
+const bootServer = () => {
+  // Body Parser: Convert input from user into object
+  app.use(express.json())
+
+  // Cors: connect client and server
+  app.use(cors())
+
+  const port = process.env.APP_PORT
+
+  app.get('/', (req, res, next) => {
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        reservations: [
+          {
+            content: 'Hello World',
+            date: '9/3/2022',
+          },
+        ],
+      },
+    })
   })
-})
 
-const port = 5000
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+  })
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+// Connect to Database MongoDB
+// Then boot the server
+const { connectDB } = require('./config/db')
+connectDB()
+  .then(() => console.log('DB connection successfully'))
+  .then(() => bootServer())
