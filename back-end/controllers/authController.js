3e3e1a0cf350fcc6045d3dd51335b6ed28f40b5b
Terry <71194512+terrytamhoang@users.noreply.github.com>
2022-11-23 05:38:31 -0600

    Auth Login Route, Auth Login Controller

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 // Need to use async/await because of connecting with DABATSE
 exports.register = async (req, res, next) => {
@@ -20,4 +21,30 @@ exports.register = async (req, res, next) => {
   } catch (error) {
     res.json(error)
   }
+}
+
+exports.login = async (req, res, next) => {
+  try {
+    // find user in DB with real email from user input (req.body.email)
+    const user = await User.findOne({ email: req.body.email })
+
+    if (!user) {
+      //Error: Email is not correct
+    }
+
+    // compare 2 password
+    // req.body.password: real password client input
+    // user.password: password in DATABSE
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      const token = jwt.sign({ userId: user._id }, process.env.APP_SECRET)
+
+      // response back to client if successfully creating the user
+      res.status(200).json({
+        status: 'Success',
+        data: { token, user, userName: user.name },
+      })
+    } else {
+      //Error: Password is not correct
+    }
+  } catch (error) {}
 }
--- back-end/routes/authRoute.js
+++ back-end/routes/authRoute.js
@@ -3,8 +3,10 @@ const express = require('express')
 const Router = express.Router()
 
 // Connect to controller
-const { register } = require('../controllers/authController')
+const { register, login } = require('../controllers/authController')
 
 Router.route('/register').post(register)
+
+Router.route('/login').post(login)
 
 module.exports = Router
