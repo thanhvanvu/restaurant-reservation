c5b96a89f2d8c667efcb8c6d57bb8cbdff9b13b5
Terry <71194512+terrytamhoang@users.noreply.github.com>
2022-11-27 09:28:56 -0600

    Login function

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -1,25 +1,100 @@
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import AppContext from '../AppContext'
+import { useContext, useState } from 'react'
 
 export default function Login() {
+  // useContext: to get the DISPATCH from AppContext
+  const { dispatch } = useContext(AppContext)
+
+  //#region --- Get User Input ---
+  // initial state is empty
+  const [userInput, setUserInput] = useState({ email: '', password: '' })
+  const onChangeHandler = (e) => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value })
+  }
+  //#endregion
+
+  const navigate = useNavigate()
+
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const onSubmitHandler = async (e) => {
+    try {
+      // prevent page will refresh
+      e.preventDefault()
+
+      const options = {
+        method: 'post',
+        url: '/api/v1/auth/login',
+        data: userInput,
+      }
+
+      const response = await axios(options)
+      console.log(response)
+
+      // get TOKEN, userName
+      const { token, userName, user } = response.data.data
+
+      // save TOKEN in localstorage
+      localStorage.setItem('token', token)
+
+      // Use DISPATCH to update the initial state
+      dispatch({ type: 'CURRENT_USER', payload: { userName, user } })
+
+      // Navigate to homepage after login
+      navigate('/')
+    } catch (error) {
+      setErrorMessage(error.response.data.message)
+    }
+  }
+
+  //#region --- Show/Hide password---
+  const [passwordShown, setPasswordShown] = useState(false)
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown)
+  }
+  //#endregion
+
   return (
     <div id="hero" className="container has-two-col">
       <div id="login">
-        <form id="login-form">
+        <form id="login-form" onSubmit={onSubmitHandler}>
           <div id="form-name">
             <h2>Login Form</h2>
           </div>
 
           <div id="id-password-submit">
-            <div id="error-massage">Error:</div>
+            {errorMessage && <div id="error-massage">{errorMessage}</div>}
 
             <div id="email">
-              <input type="email" name="email" placeholder="Email" required />
+              <input
+                type="email"
+                name="email"
+                value={userInput.email}
+                onChange={onChangeHandler}
+                placeholder="Email"
+                required
+              />
             </div>
 
             <div id="password">
-              <input name="password" placeholder="Password" required />
-              <span id="show-password">Show</span>
+              <input
+                type={passwordShown ? 'text' : 'password'}
+                name="password"
+                value={userInput.password}
+                onChange={onChangeHandler}
+                placeholder="Password"
+                required
+              />
+              <span
+                id="show-password"
+                onClick={togglePassword}
+                className="prevent-select"
+              >
+                {passwordShown ? 'Hide' : 'Show'}
+              </span>
             </div>
 
             <div id="signin">
--- front-end/src/reducers/AppReducer.js
+++ front-end/src/reducers/AppReducer.js
@@ -1,1 +1,11 @@
-export default function reducer(state, action) {}
+export default function reducer(state, action) {
+  switch (action.type) {
+    case 'CURRENT_USER':
+      return {
+        ...state,
+        user: action.payload,
+      }
+    default:
+      return state
+  }
+}
